f6a046ac1f15767671b28e6ca2dc21d51272dd3c
Ikamari <SilverFire098@gmail.com>
2019-11-05 23:54:26 +0200

    fix in localhost check + check will work only in prod env

--- app/controllers/ServerController.js
+++ app/controllers/ServerController.js
@@ -6,15 +6,17 @@ class ServerController extends Controller {
 
     constructor(skipRoutesCreation = false) {
         super(true);
-        this.router.use(ServerController.checkWhetherLocalhost);
+        if (process.env.NODE_ENV === "prod") {
+            this.router.use(ServerController.checkWhetherLocalhost);
+        }
         if (!skipRoutesCreation) {
             this.createRoutes();
         }
     }
 
     static checkWhetherLocalhost(request, response, next) {
-        const ip = request.connection.remoteAddress;
-        if (ip !== "127.0.0.1") {
+        const ip = request.connection.remoteAddress.replace(/^.*:/, "");
+        if (ip !== "1") {
             // todo: save logs to file
             console.log(`${ip} tried to access endpoint for game server and was refused`);
             return next(new ErrorResponse("Forbidden", 403));
